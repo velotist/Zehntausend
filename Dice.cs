079c994092681ddff1a0189247b67eda8aef573f
Marcus Greiner <myeyetea@posteo.de>
2020-07-21 09:28:09 +0200

    Added class Dice with mehtod for dicing one dice Included user stories for Iteration #2

---
+++ Dice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zehntausend
+{
+    class Dice
+    {
+        public int Eyes { get; }
+
+        private int DiceEye(int numberOfEyes)
+        {
+            Random random = new Random();
+            int eye = random.Next(1, numberOfEyes);
+
+            return eye;
+        }
+    }
+}
